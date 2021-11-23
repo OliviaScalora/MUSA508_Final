28b2213e7a2ea0c3b72393ff517b62ae39694843
OliviaScalora <oscalora@design.upenn.edu>
2021-11-22 22:39:34 -0500

    Update 508Final_Script.R

--- 508Final_Script.R
+++ 508Final_Script.R
@@ -27,6 +27,10 @@ color2 = '#a32714'
 color3 = '#539470'
 
 #load data
+tracts17 <- get_acs(geography = "tract", variables = c("B25026_001E","B19013_001E","B25058_001E",
+                                                       "B06012_002E", "B25003_003E", "B25003_002E", "B25004_001E" ), 
+                    year=2017, state=04, county=013, geometry=T,output='wide') %>% 
+  st_transform('EPSG:2224')
 
 opioid_data <- st_read("https://raw.githubusercontent.com/OliviaScalora/MUSA508_Final/main/Data/Fire_and_Medical_Opioid_Overdose_Incidents.csv")
 
@@ -38,8 +42,31 @@ zoning_district <- st_read("https://raw.githubusercontent.com/OliviaScalora/MUSA
 
 bus_ridership<- st_read("https://raw.githubusercontent.com/OliviaScalora/MUSA508_Final/main/Data/Bus_Ridership.csv")
 
-city_boundary<- st_read("https://raw.githubusercontent.com/OliviaScalora/MUSA508_Final/main/Data/City_Boundary.csv",crs = 'EPSG:4326')
-city_boundary <- city_boundary::st_as_sf( city_boundary, wkt = "geometry" )
+city_boundary<- st_read("https://raw.githubusercontent.com/OliviaScalora/MUSA508_Final/main/Data/City_Boundary.csv")
+
+city_boundary <- st_as_sf(city_boundary, wkt = 'Geometry',
+                        crs = 4326, agr = 'constant') %>%
+  st_transform(st_crs(tracts17))
+
+parcels<- st_read("https://raw.githubusercontent.com/OliviaScalora/MUSA508_Final/main/Data/City_Parcel_Polygons.csv")
+
+parcels<- st_as_sf(parcels, wkt = 'Geometry',
+                          crs = 4326, agr = 'constant') %>%
+  st_transform(st_crs(tracts17))
+
+ggplot()+ geom_sf(data=city_boundary,
+                  color = 'red',
+                  size = 1)+
+            geom_sf(data=parcels)
+ 
+fishnet <- 
+  st_make_grid(city_boundary,
+               cellsize = 500, 
+               square = TRUE) %>%
+  .[city_boundary] %>%            # <- MDH Added
+  st_sf() %>%
+  mutate(uniqueID = rownames(.))
+
 
 
 glimpse(city_boundary)
